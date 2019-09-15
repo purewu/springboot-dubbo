fb9cfe0230a58c4e524de784fe573e8056ba5ed8
tom_glb <tom_gla123@163.com>
2019-09-15 16:41:17 +0800

    hello dev

---
+++ provider/src/main/java/com/wudepeng/provider/Hello.java
@@ -0,0 +1,12 @@
+package com.wudepeng.provider;
+
+/**
+ * @Description
+ * @Author wudepeng
+ * @Date 2019/9/15 16:37
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello dev!");
+    }
+}
